f02761ceb95444581bd63955718df2be27e5feed
Abraão Araujo <96948947+Abraao4raujo@users.noreply.github.com>
2023-05-10 12:25:38 -0300

    input limited 0 at 10

--- alunos.js
+++ alunos.js
@@ -5,42 +5,53 @@ const span = document.getElementById('span')
 const clear = document.getElementById('limparDados')
 
 function getDataAv1() {
-  const notaAv1 = parseFloat(document.getElementById('notaAv1Aluno').value)
-  const notaAv2 = parseFloat(document.getElementById('notaAv2Aluno').value)
+  const notaAv1s = parseFloat(document.getElementById('notaAv1Aluno').value)
+  const notaAv2s = parseFloat(document.getElementById('notaAv2Aluno').value)
 
-  if (isNaN(notaAv1) && isNaN(notaAv2)) {
-    span.innerHTML = ''
+  if (notaAv1s > notaAv1.getAttribute('max')) {
+    /* Caso seja, define o valor do atributo max */
+    notaAv1.value = 1
+  } else if (notaAv2s > notaAv2.getAttribute('max')) {
+    notaAv2.value = 1
   }
+  else {
 
-  if (!isNaN(notaAv1) && isNaN(notaAv2)) {
-    const resultado = (7 * 2) - notaAv1
-    const final = 8 - notaAv1
-
-    if (notaAv1 <= 3) {
-      span.innerHTML = `VOCÊ NÃO TEM CONDIÇÕES DE PASSAR DIRETO.
-    PARA IR A FINAL, VOCÊ PRECISA TIRAR NOTA IGUAL A ${final} NA SEGUNDA AVALIAÇÃO.`
-      span.classList.add('reprovado')
-      span.classList.remove('aprovado')
-    } else {
-      span.innerHTML = `VOCÊ PRECISA TIRAR ${resultado} PTS PARA SER APROVADO.`
-      span.classList.remove('aprovado', 'reprovado')
+    if (isNaN(notaAv1) && isNaN(notaAv2)) {
+      span.innerHTML = ''
     }
-  } else if (!isNaN(notaAv1) && !isNaN(notaAv2)) {
-    const somaNotas = (notaAv1 + notaAv2)
-    const mediaNotas = somaNotas / 2
 
-    if (somaNotas >= 14) {
-      span.innerHTML = `Parabéns! Você passou com uma média de ${mediaNotas}`
-      span.classList.add('aprovado')
-      span.classList.remove('reprovado')
-    } else {
-      span.innerHTML = `Infelizmente, você não passou. Sua média foi ${mediaNotas}.`
-      span.classList.add('reprovado')
-      span.classList.remove('aprovado')
+    if (!isNaN(notaAv1s) && isNaN(notaAv2s)) {
+      const resultado = (7 * 2) - notaAv1s
+      const final = 8 - notaAv1s
+
+      if (notaAv1s <= 3) {
+        span.innerHTML = `VOCÊ NÃO TEM CONDIÇÕES DE PASSAR DIRETO.
+    PARA IR A FINAL, VOCÊ PRECISA TIRAR NOTA IGUAL A ${final} NA SEGUNDA AVALIAÇÃO.`
+        span.classList.add('reprovado')
+        span.classList.remove('aprovado')
+      } else {
+        span.innerHTML = `VOCÊ PRECISA TIRAR ${resultado} PTS PARA SER APROVADO.`
+        span.classList.remove('aprovado', 'reprovado')
+      }
+    } else if (!isNaN(notaAv1s) && !isNaN(notaAv2s)) {
+      const somaNotas = (notaAv1s + notaAv2s)
+      const mediaNotas = somaNotas / 2
+
+      if (somaNotas >= 14) {
+        span.innerHTML = `Parabéns! Você passou com uma média de ${mediaNotas}`
+        span.classList.add('aprovado')
+        span.classList.remove('reprovado')
+      } else {
+        span.innerHTML = `Infelizmente, você não passou. Sua média foi ${mediaNotas}.`
+        span.classList.add('reprovado')
+        span.classList.remove('aprovado')
+      }
     }
   }
 }
 
+notaAv1.addEventListener('click', getDataAv1)
+notaAv2.addEventListener('click', getDataAv1)
 notaAv1.addEventListener('keyup', getDataAv1)
 notaAv2.addEventListener('keyup', getDataAv1)
 clear.addEventListener('click', () => {
